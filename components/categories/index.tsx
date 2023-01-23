c81dd78e9dfa64a7e60b5c937ad78dc25ae75dbb
ceky <cekyy07@gmail.com>
2023-01-24 00:07:48 +0200

    Add a selecte menu for categories

--- components/categories/index.tsx
+++ components/categories/index.tsx
@@ -1,26 +1,46 @@
 "use client";
 
-import { useEffect, useCallback } from "react";
+import { useState, useEffect, useCallback } from "react";
 import { NextPage } from "next";
+import { Select } from "antd";
 
 import supabase from "../../utils/supabase";
 
+interface Category {
+  value: string;
+  label: string;
+}
+
 const Categories: NextPage = () => {
+  const [categories, setCategories] = useState<Category[]>([]);
+
   const getCategories = useCallback(async () => {
-    let { data: categories, error } = await supabase
-      .from("categories")
-      .select("name");
-    console.info(categories);
+    let { data } = await supabase.from("categories").select("name");
+
+    const loadedCategories: Category[] = data!.map((category) => ({
+      value: category.name,
+      label: category.name,
+    }));
+    setCategories(loadedCategories);
   }, []);
 
   useEffect(() => {
     getCategories();
   }, [getCategories]);
 
+  const handleChange = (category: string) => {
+    console.info(category);
+  };
+
+  if (!categories.length) return null;
+
   return (
-    <>
-      <div>categories</div>
-    </>
+    <Select
+      style={{ width: 120 }}
+      defaultValue={categories[0].value}
+      onChange={(category) => handleChange(category)}
+      options={categories}
+    />
   );
 };
 
