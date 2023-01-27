9a07ea4c2e4b96c46649ca53b07ca589ff0b1504
ceky <cekyy07@gmail.com>
2023-01-27 23:28:27 +0200

    Render posts

--- components/posts/index.tsx
+++ components/posts/index.tsx
@@ -4,16 +4,26 @@ import { NextPage } from "next";
 import { useAtom } from "jotai";
 import supabase from "@/utils/supabase";
 import { categoryAtom } from "@/context/state";
-import { useCallback, useEffect } from "react";
+import { useCallback, useEffect, useState } from "react";
+
+interface Post {
+  id: number;
+  created_at: string;
+  title: string;
+  description: string;
+  link: string;
+}
 
 const Posts: NextPage = () => {
   const [selectedCategory] = useAtom(categoryAtom);
+  const [posts, setPosts] = useState<Post[]>([]);
 
   const loadPosts = useCallback(async () => {
     let { data } = await supabase
       .from(selectedCategory.toLowerCase())
       .select("*");
     console.log(JSON.stringify(data));
+    setPosts(data as Post[]);
   }, [selectedCategory]);
 
   useEffect(() => {
@@ -22,7 +32,25 @@ const Posts: NextPage = () => {
     loadPosts();
   }, [loadPosts, selectedCategory]);
 
-  return <div>{selectedCategory}</div>;
+  const handlePostClick = useCallback((link: string) => {
+    window.open(link, "_blank");
+  }, []);
+
+  return (
+    <div>
+      <p>{selectedCategory}</p>
+      {posts.length > 0 && (
+        <ul>
+          {posts.map((post) => (
+            <li key={post.id} onClick={() => handlePostClick(post.link)}>
+              <h2>{post.title}</h2>
+              <p>{post.description}</p>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
 };
 
 export default Posts;
