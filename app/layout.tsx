3351774936d0d03ae1a8752fd5f0ef47c2e88497
ceky <cekyy07@gmail.com>
2023-01-29 18:17:19 +0200

    Deploy to netlify

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,14 +14,14 @@ export default function RootLayout({
         <link rel="icon" href="/favicon.ico" />
 
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
           href="https://fonts.googleapis.com/css2?family=Lato&display=swap"
           rel="stylesheet"
         />
 
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
           href="https://fonts.googleapis.com/css2?family=Catamaran&display=swap"
           rel="stylesheet"
